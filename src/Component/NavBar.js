5bb99aa7185a2017e6a20c3c270dd033b88a72c8
IgnacioMarelli <ignacio.marelli@hotmail.com>
2022-03-22 16:06:30 -0300

    menu

---
+++ src/Component/NavBar.js
@@ -0,0 +1,15 @@
+import './NavBar.css';
+
+const NavBar = ()=> {
+    return <nav>
+        <a className='Logo'>Carnes Avenida</a>
+        <ul >
+            <a><li>Ternera</li></a>
+            <a><li>Cerdo</li></a>
+            <a><li>Embutido</li></a>
+            <a><li>Achuras</li></a>
+        </ul>
+        </nav>
+}
+
+export default NavBar
